f2aa347126d9f6d0be7dd49afd4f8a655afb29f8
DIMITRIOS SIDIROPOULOS <44368252+p16sidi@users.noreply.github.com>
2018-11-02 17:06:35 +0200

    Update script_2.js

--- assets/scripts/script_2.js
+++ assets/scripts/script_2.js
@@ -11,7 +11,7 @@ var margin = {top: 30, right: 0, bottom: 20, left: 0},
     formatNumber = d3.format(","),
     transitioning;
 
-var color = d3.scaleLinear().domain([0, 1/4*5000000, 2/4*5000000, 3/4*5000000, 5000000])..range(["#ef9999", "#e8b9ae", "#d8cdb3", "#90afa2", "#6f94a3"]);
+var color = d3.scaleLinear().domain([0, 1/4*5000000, 2/4*5000000, 3/4*5000000, 5000000]).range(["#ef9999", "#e8b9ae", "#d8cdb3", "#90afa2", "#6f94a3"]);
 //.range(["#73c3bf", "#a1d8c8", "#cbe0a7", "#f2db84"]);
 
 // sets x and y scale to determine size of visible boxes
